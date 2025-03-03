5fa4e2979aedd55a1fae5b91004c428fe07573d7
zkan <zkan.cs@gmail.com>
2025-03-03 17:31:32 +0700

    Add test valid book

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid book" do
+    book = Book.new(
+      title: "Seeing the Forest for Trees: A Manager's Guide to Applying Systems Thinking",
+      author: "Dennis Sherwoord",
+      isbn: "978-1857883114",
+      publisher: "Nicholas Brealey Publishing"
+    )
+    assert book.valid?
+  end
 end
