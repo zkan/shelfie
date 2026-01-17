72348d9c241da72e70e007feab396c7321af4f0a
zkan <zkan.cs@gmail.com>
2026-01-17 10:16:59 +0700

    Refresh lock and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_17_012905) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_17_012905) do
   create_table "books", force: :cascade do |t|
-    t.string "title"
     t.string "author"
-    t.string "isbn"
-    t.string "publisher"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "format"
+    t.string "isbn"
     t.integer "publication_year"
+    t.string "publisher"
+    t.string "title"
+    t.datetime "updated_at", null: false
   end
 end
